3aa2ab5728b1b9e03e4ae8581bbda4f829a81864
Victor Macias <v.maciascastillo@gmail.com>
2016-11-21 14:32:42 +0100

    inteligent coach

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,8 +1,21 @@
 class CoachingController < ApplicationController
-  def answer
-  	@answer = params[:query]
-  end
+	def answer
+		@question = params[:query]
+		@answer = coach_answer(@question)
+	end
 
-  def ask
-  end
+	def ask
+	end
+
+	def coach_answer(your_message)
+		if your_message == "I AM GOING TO WORK RIGHT NOW!"
+			return ""
+		elsif your_message == your_message.upcase && (your_message.include? "?")
+			return "I can feel your motivation! Silly question, get dressed and go to work!"
+		elsif your_message == your_message.upcase
+			return "I can feel your motivation! I don't care, get dressed and go to work!"
+		else
+			return "I don't care, get dressed and go to work!"
+		end
+	end
 end
